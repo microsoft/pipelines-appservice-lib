bfee212a379735e4931a5b38754757899ef77401
Sumiran Aggarwal <suaggar@microsoft.com>
2019-10-04 14:36:47 +0530

    pr comments

--- packages/secret-parser/src/index.ts
+++ packages/secret-parser/src/index.ts
@@ -8,6 +8,10 @@ export enum FormatType {
     "XML"
 }
 
+/**
+ * Takes content as string and format type (xml, json).
+ * Exposes getSecret method to get value of specific secret in object and set it as secret.
+ */
 export class SecretParser {
     private dom: string;
     private contentType: FormatType;
@@ -36,7 +40,14 @@ export class SecretParser {
         this.contentType = contentType;
     }
 
-    public getSecret(key: string, isSecret: boolean = false, variableName?: string): string {
+    /**
+     * 
+     * @param key jsonpath or xpath depending on content type
+     * @param isSecret should the value parsed be a secret. Deafult: true
+     * @param variableName optional. If provided value will be exported with this variable name
+     * @returns a string value or empty string if key not found
+     */
+    public getSecret(key: string, isSecret: boolean = true, variableName?: string): string {
         let value: string = "";
         switch(this.contentType) {
             case FormatType.JSON:
