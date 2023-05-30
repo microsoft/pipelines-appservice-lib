7b72d078fdbab56a5819dc0ed241017e2378b19a
sgollapudi <85578033+sgollapudi77@users.noreply.github.com>
2023-05-30 15:45:15 +0530

    Added script to copy modules to web-apps-deploy

--- packages/CopyModules.ps1
+++ packages/CopyModules.ps1
@@ -3,4 +3,11 @@ echo "Current Directory: $PWD"
 npm run build
 Copy-Item -Path "C:\Users\sgollapudi\Documents\Work\LinuxGithubAction\pipelines-appservice-lib\packages\appservice-rest\lib\*" -Destination "C:\Users\sgollapudi\Documents\Work\LinuxGithubAction\webapps-deploy\node_modules\azure-actions-appservice-rest\" -Recurse -Force
 
-# Set-Location "C:\Users\sgollapudi\Documents\Work\LinuxGithubAction\webapps-deploy"
+Set-Location "C:\Users\sgollapudi\Documents\Work\LinuxGithubAction\webapps-deploy"
+echo "Current Directory: $PWD"
+
+git add *
+git commit -m "Copying built node_modules to webapps-deploy"
+git push
+
+Set-Location "C:\Users\sgollapudi\Documents\Work\LinuxGithubAction\pipelines-appservice-lib\packages\"
