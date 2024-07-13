233c4769acf97f9982538e6fbe9e2c21259174a3
Shilpi Rachna <shilpirachna.ms@gmail.com>
2024-07-13 14:15:26 +0530

    Added support to fetch slot app service detail

--- packages/appservice-rest/src/Utilities/AzureResourceFilterUtility.ts
+++ packages/appservice-rest/src/Utilities/AzureResourceFilterUtility.ts
@@ -2,13 +2,18 @@ import { IAuthorizer } from 'azure-actions-webclient/Authorizer/IAuthorizer';
 import { Resources } from '../Arm/azure-arm-resource';
 
 export class AzureResourceFilterUtility {
-    public static async getAppDetails(endpoint: IAuthorizer, resourceName: string, resourceGroupName?: string): Promise<any> {
+    public static async getAppDetails(endpoint: IAuthorizer, resourceName: string, resourceGroupName?: string, slotName?: string): Promise<any> {
         var azureResources: Resources = new Resources(endpoint);
-        var filteredResources: Array<any> = await azureResources.getResources('Microsoft.Web/Sites', resourceName, resourceGroupName);
+        var resourceType: string = 'Microsoft.Web/Sites'
+        if (!!slotName){
+            resourceType = 'Microsoft.Web/Sites/Slots'
+            resourceName = resourceName.concat("/", slotName)
+        }
+        var filteredResources: Array<any> = await azureResources.getResources(resourceType, resourceName, resourceGroupName);
         let rgName: string;
         let kind: string;
         if(!filteredResources || filteredResources.length == 0) {
-            throw new Error(`Resource ${resourceName} doesn't exist.`);
+            throw new Error(`Resource ${resourceName} of type ${resourceType} doesn't exist.`);
         }
         else if(filteredResources.length == 1) {
             rgName = filteredResources[0].id.split("/")[4];
